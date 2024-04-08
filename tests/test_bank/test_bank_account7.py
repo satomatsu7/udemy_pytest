f80b4c1e989fe7f29a8e0f0a9c75277d7d714b77
satoshi.A <sato.matsu.shinobu@gmail.com>
2024-04-08 11:49:00 +0900

    update

--- tests/test_bank/test_bank_account7.py
+++ tests/test_bank/test_bank_account7.py
@@ -6,8 +6,3 @@ import os
 def test_feature_x():
     # 未実装のテスト
     pass
-
-# @pytest.mark.skipif(os.getenv("MY_ENV_VAR") == "true", reason="MY_ENV_VARがtrueの場合はスキップ")
-# def test_feature_y():
-#     # MY_ENV_VARがtrueのときにスキップされるテスト
-#     pass
